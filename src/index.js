e001410826a12ad29d5d495c1de24631eb3e5731
Chris Agold <christophersagold@gmail.com>
2021-06-02 12:36:20 -0500

    imported calculator methods

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import * as pi from "./math.js";
+import * as cal from "./calculator.js";
 
 ReactDOM.render(
     // <App/>,
+    // <ul>
+    //     <li>{pi.default}</li>
+    //     <li>{pi.doublePi()}</li>
+    //     <li>{pi.triplePi()}</li>
+    // </ul>,
     <ul>
-        <li>{pi.default}</li>
-        <li>{pi.doublePi()}</li>
-        <li>{pi.triplePi()}</li>
+        <li>{cal.add(1, 3)}</li>
+        <li>{cal.multiply(5, 5)}</li>
+        <li>{cal.subtract(10, 2)}</li>
+        <li>{cal.divide(25, 5)}</li>
     </ul>,
     document.getElementById('root')
 );
