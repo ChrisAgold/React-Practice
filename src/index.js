ce5ecbc9c591822eb6d15ce1c6419d0bd258e15b
Chris Agold <christophersagold@gmail.com>
2021-06-02 12:27:49 -0500

    added import and export notes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import * as pi from "./math.js";
 
 ReactDOM.render(
-    <App/>,
+    // <App/>,
+    <ul>
+        <li>{pi.default}</li>
+        <li>{pi.doublePi()}</li>
+        <li>{pi.triplePi()}</li>
+    </ul>,
     document.getElementById('root')
 );
 
