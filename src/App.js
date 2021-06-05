52dd6bea1e09368f724cdece33454ecad438004e
Chris Agold <christophersagold@gmail.com>
2021-06-05 11:02:29 -0500

    added filter functinality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React, {Component} from 'react';
-import CardList from "./CardList";
+import React, { Component } from 'react';
+import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from "./robots";
+// import Scroll from './Scroll';
+// import './App.css';
 
 class App extends Component {
-    constructor () {
+    constructor() {
         super()
         this.state = {
-            //    Robots array
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-    render() {
-        return (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox/>
-                <CardList robots={this.state.robots}/>
-            </div>
-        );
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => {this.setState({ robots: users})});
     }
 
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                        <CardList robots={filteredRobots} />
+                </div>
+            );
+    }
 }
 
 export default App;
