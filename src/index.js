1e4eb221133dafeb068cb4fa2c21e21eaf5df374
Chris Agold <christophersagold@gmail.com>
2021-06-08 10:35:30 -0500

    added contacts

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-    return (
-        <div className="my-style">
-            <h2>{props.name}</h2>
-            <img
-                src={props.img}
-                alt="avatar"
-            />
-            <p>{props.tel}</p>
-            <p>{props.email}</p>
-        </div>
-    );
-}
-
-ReactDOM.render(
-    <div>
-        <h1>My Contacts</h1>
-        <Card name="Beyonce"
-              img=""
-              tel="+123 456 789" email="b@beyonce.com"/>
-        <input id="fName"/>
-
-        <Card
-            name="Jack Bauer"
-            img=""
-            tel="7307676555"
-            email="jack@gmail.com"
-        />
-    </div>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
