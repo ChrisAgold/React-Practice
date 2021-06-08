475561959a2c881d233028270620381aee3bec61
Chris Agold <christophersagold@gmail.com>
2021-06-08 10:13:43 -0500

    added card props

--- src/index.js
+++ src/index.js
@@ -2,24 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Card(props) {
-    return <div>
-        <h2>{props.name}</h2>
-        <img
-            src={props.img}
-            alt="avatar"
-        />
-        <p>{props.tel}</p>
-        <p>{props.email}</p>
-    </div>
+    return (
+        <div className="my-style">
+            <h2>{props.name}</h2>
+            <img
+                src={props.img}
+                alt="avatar"
+            />
+            <p>{props.tel}</p>
+            <p>{props.email}</p>
+        </div>
+    );
 }
 
 ReactDOM.render(
     <div>
         <h1>My Contacts</h1>
         <Card name="Beyonce"
-              img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+              img=""
               tel="+123 456 789" email="b@beyonce.com"/>
         <input id="fName"/>
+
+        <Card
+            name="Jack Bauer"
+            img=""
+            tel="7307676555"
+            email="jack@gmail.com"
+        />
     </div>,
     document.getElementById("root")
 );
