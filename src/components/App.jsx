e9af11a9cfa64f740b2b228bc4618707d2fb7827
Chris Agold <christophersagold@gmail.com>
2021-06-09 10:28:50 -0500

    added props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import Card from "./Card.jsx"
-
+import contacts from "../contacts";
+console.log(contacts);
 function App() {
     return (
         <div>
             <Card
-                name="Kirishima"
-                img="circle-img"
-                src="https://www.anime-planet.com/images/characters/eijiro-kirishima-66853.jpg"
-                alt="avatar_img"
-                tel="+123 456 789"
-                email="k@kiri.com"
+                name={contacts[0].name}
+                img={contacts[0].imgURL}
+                tel={contacts[0].phone}
+                email={contacts[0].email}
+            />
+            <Card
+                name={contacts[1].name}
+                img={contacts[1].imgURL}
+                tel={contacts[1].phone}
+                email={contacts[1].email}
+            />
+            <Card
+                name={contacts[2].name}
+                img={contacts[2].imgURL}
+                tel={contacts[2].phone}
+                email={contacts[2].email}
             />
         </div>
     );
