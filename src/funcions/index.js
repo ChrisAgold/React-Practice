202a734aebb99bb4965f5171d099c142d8592c9a
Chris Agold <christophersagold@gmail.com>
2021-06-09 14:17:06 -0500

    added function examples

---
+++ src/funcions/index.js
@@ -0,0 +1,53 @@
+// function double(x) {
+//     return x * 2;
+// }
+// const newNumbers = numbers.map(double);
+
+// var newNumbers = [];
+// numbers.forEach(function (x) {
+//     newNumbers.push(x * 2);
+// });
+
+// const newNumbers = numbers.map(function (x) {
+//     return x * 2;
+// });
+// console.log(newNumbers);
+
+// Filter - Create a new array keeping the items that return true;
+
+// const newNumbers = numbers.filter(function(num) {
+//     return num < 10
+// });
+
+// var newNumbers = [];
+// numbers.forEach(function(num){
+//     if(num < 10) {
+//         newNumbers.push(num);
+//     }
+// });
+
+// Reduce - Accumulate a value by doing something to each item in an array.
+
+// var newNumber = 0;
+// numbers.forEach(function(curNumber) {
+// newNumber += curNumber;
+// });
+
+// var newNumber = numbers.reduce(function(accumulator, currentNumber) {
+// return accumulator + currentNumber;
+// });
+
+// Find - find the first item that matches from an array
+
+// const newNumber = numbers.find(function(num) {
+//     return num > 10;
+//     });
+
+// FindIndex - find the index of the first item that matches
+// var numbers = [3, 56, 2, 48, 5];
+//
+// const newNumber = numbers.findIndex(function(num) {
+//     return num > 10;
+// });
+//
+// console.log(newNumber);
