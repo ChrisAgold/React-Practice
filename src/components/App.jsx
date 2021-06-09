1b15d4c306de0453e7c274fe07f363e938cacfcc
Chris Agold <christophersagold@gmail.com>
2021-06-09 16:32:07 -0500

    created login component

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Login from './Login.jsx'
+
+var isLoggedIn = true;
+
+function renderConditionally() {
+    if (isLoggedIn) {
+        return <h1>Hello</h1>
+    } else {
+        return <Login />
+    }
+}
+
+function App() {
+    return <div className="container" >
+        {renderConditionally()}
+    </div>;
+}
+
+export default App;
--- src/funcions/index.js
+++ src/funcions/index.js
@@ -1,64 +1,13 @@
-// function double(x) {
-//     return x * 2;
-// }
-// const newNumbers = numbers.map(double);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-// var newNumbers = [];
-// numbers.forEach(function (x) {
-//     newNumbers.push(x * 2);
-// });
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// const newNumbers = numbers.map(function (x) {
-//     return x * 2;
-// });
-// console.log(newNumbers);
+var numbers = [3, 56, 2, 48, 5];
 
-// Filter - Create a new array keeping the items that return true;
-
-// const newNumbers = numbers.filter(function(num) {
-//     return num < 10
-// });
-
-// var newNumbers = [];
-// numbers.forEach(function(num){
-//     if(num < 10) {
-//         newNumbers.push(num);
-//     }
-// });
-
-// Reduce - Accumulate a value by doing something to each item in an array.
-
-// var newNumber = 0;
-// numbers.forEach(function(curNumber) {
-// newNumber += curNumber;
-// });
-
-// var newNumber = numbers.reduce(function(accumulator, currentNumber) {
-// return accumulator + currentNumber;
-// });
-
-// Find - find the first item that matches from an array
-
-// const newNumber = numbers.find(function(num) {
-//     return num > 10;
-//     });
-
-// FindIndex - find the index of the first item that matches
-// var numbers = [3, 56, 2, 48, 5];
-//
-// const newNumber = numbers.findIndex(function(num) {
-//     return num > 10;
-// });
-//
-// console.log(newNumber);
-
-import emojipedia from "./emojipedia";
-
-const newEmojipedia = emojipedia.map(function (emojiEntry) {
-    return emojiEntry.meaning.substring(0, 100);
+const newNumbers = numbers.map( (x) => {
+    return x * x;
 });
 
-console.log(newEmojipedia);
-
-
-
+console.log(newNumbers);
