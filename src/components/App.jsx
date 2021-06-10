a5f53ec2279f8a247be2166948652e6fdfc2d186
Chris Agold <christophersagold@gmail.com>
2021-06-10 15:51:20 -0500

    added form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,23 @@ import React, {useState} from "react";
 
 function App() {
 
-    const [headingText, setHeadingText] = useState("Hello");
-    const [isMousedOver, setMouseOver] = useState(false);
+    const [name, setName] = useState("");
 
-    function handleClick() {
-        setHeadingText("Submitted");
-    }
-
-    function handleMouseOver() {
-        setMouseOver(true);
-    }
-
-    function handleMouseOut() {
-        setMouseOver(false);
+    function handleChange(event) {
+        console.log(event.target.value);
+        setName(event.target.value);
     }
 
     return (
         <div className="container">
-            <h1>{headingText}</h1>
-            <input type="text" placeholder="What's your name?"/>
-            <button style={{backgroundColor: isMousedOver ? "black" : "white"}}
-                    onClick={handleClick}
-                    onMouseOver={handleMouseOver}
-                    onMouseOut={handleMouseOut}
-            >
+            <h1>Hello {name}</h1>
+            <input
+                onChange={handleChange}
+                type="text"
+                placeholder="What's your name?"
+                value={name}
+            />
+            <button>
                 Submit
             </button>
         </div>
