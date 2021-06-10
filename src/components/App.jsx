24f908e2a3d365979580ae27887cba4801111e63
Chris Agold <christophersagold@gmail.com>
2021-06-10 09:10:15 -0500

    added conditional statement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import Login from './Login.jsx'
+import Login from './Login.jsx';
+import Form from './Form.jsx';
 
 var isLoggedIn = false;
+var userIsRegistered = false;
 
-const currentTime = new Date(2021, 6, 9).getHours();
-console.log(currentTime);
+// const currentTime = new Date(2021, 6, 9).getHours();
+// console.log(currentTime);
 
 function App() {
     return <div className="container"> {
         // isLoggedIn === true ? <h1>Hello</h1> : <Login/>
         // can also be written as this:
         // isLoggedIn ? <h1>Hello</h1> : <Login/>
-        currentTime > 12 && <h1>Why are you still working?</h1>
+        // currentTime > 12 && <h1>Why are you still working?</h1>
+        <Form
+            isRegistered={userIsRegistered}
+        />
     }</div>;
 }
 
