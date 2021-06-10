c081e65f1059d97771974cbd194e3d6686a3a5c5
Chris Agold <christophersagold@gmail.com>
2021-06-10 08:57:16 -0500

    added terinary statements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import Login from './Login.jsx'
 
-var isLoggedIn = true;
+var isLoggedIn = false;
 
-function renderConditionally() {
-    if (isLoggedIn) {
-        return <h1>Hello</h1>
-    } else {
-        return <Login />
-    }
-}
+const currentTime = new Date(2021, 6, 9).getHours();
+console.log(currentTime);
 
 function App() {
-    return <div className="container" >
-        {renderConditionally()}
-    </div>;
+    return <div className="container"> {
+        // isLoggedIn === true ? <h1>Hello</h1> : <Login/>
+        // can also be written as this:
+        // isLoggedIn ? <h1>Hello</h1> : <Login/>
+        currentTime > 12 && <h1>Why are you still working?</h1>
+    }</div>;
 }
 
 export default App;
