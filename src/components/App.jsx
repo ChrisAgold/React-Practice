1c25b8f91bed24f195a71379cc3510d6c0123bb3
Chris Agold <christophersagold@gmail.com>
2021-06-18 14:29:43 -0500

    added book components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,35 @@
 import React, {useState} from "react";
 
-function App() {
+// function App() {
+//
+//     const [name, setName] = useState("");
+//     const [headingText, setHeading] = useState("");
+//
+//     function handleChange(event) {
+//         console.log(event.target.value);
+//         setName(event.target.value);
+//     }
+//
+//     function handleClick() {
+//         setHeading(name);
+//     }
+//
+//     return (
+//         <div className="container">
+//             <h1>Hello {headingText}</h1>
+//             <form>
+//                 <input
+//                     onChange={handleChange}
+//                     type="text"
+//                     placeholder="What's your name?"
+//                     value={name}
+//                 />
+//                 <button type="submit" onClick={handleClick}> Submit</button>
+//             </form>
+//         </div>
+//     )
+//         ;
+// }
+//
+// export default App;
 
-    const [name, setName] = useState("");
-    const [headingText, setHeading] = useState("");
-
-    function handleChange(event) {
-        console.log(event.target.value);
-        setName(event.target.value);
-    }
-
-    function handleClick() {
-        setHeading(name);
-    }
-
-    return (
-        <div className="container">
-            <h1>Hello {headingText}</h1>
-            <input
-                onChange={handleChange}
-                type="text"
-                placeholder="What's your name?"
-                value={name}
-            />
-            <button onClick={handleClick}> Submit</button>
-        </div>
-    );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+// import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function BookList() {
+    return (
+        <section>
+            <Image/>
+            <Title/>
+            <Author/>
+        </section>
+    )
+}
+
+const Image = () => (
+    <img src='https://static.scientificamerican.com/sciam/cache/file/1DDFE633-2B85-468D-B28D05ADAE7D1AD8_source.jpg?w=590&h=800&D80F3D79-4382-49FA-BE4B4D0C62A5C3ED' alt=""/>
+);
+
+        const Title = () => <h1>Book</h1>;
+
+const Author = () => <h4>Jim Scila</h4>
+
+
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
