ac4a9e6d11db592671b7c15b7d1b1adb228222c6
Chris Agold <christophersagold@gmail.com>
2021-06-21 15:50:20 -0500

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from "react";
 import Tweet from "./components/Tweet";
-import ReactDOM from "react-dom";
 import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 import './styles/App.css';
 
 function App() {
+
     // State
     const [name, setName] = useState("Techboi");
     const [textInput, setTextInput] = useState("");
     const [tweets, setTweets] = useState([]);
+
+    useEffect(() => {
+        console.log("we run a function yay")
+    },[textInput]);
 
     return (
         <div>
