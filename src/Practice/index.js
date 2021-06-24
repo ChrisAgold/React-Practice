a6e0e5d42e9e5fb082ce96970add00824a5aa31b
Chris Agold <christophersagold@gmail.com>
2021-06-24 12:25:15 -0500

    added components

---
+++ src/Practice/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//
+// const firstBook = {
+//     img: 'https://static.scientificamerican.com/sciam/cache/file/1DDFE633-2B85-468D-B28D05ADAE7D1AD8_source.jpg?w=590&h=800&D80F3D79-4382-49FA-BE4B4D0C62A5C3ED',
+//     title: 'I Love Potatoes',
+//     author: 'Lee'
+// }
+//
+// const secondBook = {
+//     img: 'https://static.scientificamerican.com/sciam/cache/file/1DDFE633-2B85-468D-B28D05ADAE7D1AD8_source.jpg?w=590&h=800&D80F3D79-4382-49FA-BE4B4D0C62A5C3ED',
+//     title: 'I Love Tea',
+//     author: 'Lee'
+// }
+//
+// function BookList() {
+//     return (
+//         <section>
+//             <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+//             <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+//         </section>
+//     )
+// }
+//
+// const Book = (props) => {
+//     return (
+//         <article>
+//             <img src={props.img}/>
+//             <h1>{props.title}</h1>
+//             <h4>{props.author}</h4>
+//         </article>
+//     )
+// }
+//
+// ReactDOM.render(<BookList/>, document.getElementById("root"));
+//
+ReactDOM.render(<App/>, document.getElementById("root"));
+
--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-//
-// const firstBook = {
-//     img: 'https://static.scientificamerican.com/sciam/cache/file/1DDFE633-2B85-468D-B28D05ADAE7D1AD8_source.jpg?w=590&h=800&D80F3D79-4382-49FA-BE4B4D0C62A5C3ED',
-//     title: 'I Love Potatoes',
-//     author: 'Lee'
-// }
-//
-// const secondBook = {
-//     img: 'https://static.scientificamerican.com/sciam/cache/file/1DDFE633-2B85-468D-B28D05ADAE7D1AD8_source.jpg?w=590&h=800&D80F3D79-4382-49FA-BE4B4D0C62A5C3ED',
-//     title: 'I Love Tea',
-//     author: 'Lee'
-// }
-//
-// function BookList() {
-//     return (
-//         <section>
-//             <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-//             <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
-//         </section>
-//     )
-// }
-//
-// const Book = (props) => {
-//     return (
-//         <article>
-//             <img src={props.img}/>
-//             <h1>{props.title}</h1>
-//             <h4>{props.author}</h4>
-//         </article>
-//     )
-// }
-//
-// ReactDOM.render(<BookList/>, document.getElementById("root"));
-//
-ReactDOM.render(<App/>, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Nested components, React Tools
+
+function BookList() {
+    return (
+        <section>
+            <Book/>
+        </section>
+    )
+}
+
+const Book = () => {
+    return (
+        <article>
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>)
+};
+
+const Image = () => (
+    <img src="https://pbs.twimg.com/profile_images/1197056293963489280/rImVimA8_400x400.jpg"
+         alt=''
+    />
+)
+
+const Title = () => <h1>Hello</h1>;
+const Author = () => (
+    <h4 style={{color: '#617d98', fontSize: '0.75trm', marginTop: '0.25rem'}}>
+        Kirashima
+    </h4>
+)
+ReactDOM.render(<BookList/>, document.getElementById('root'));
 
