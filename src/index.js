c829a7294b794225282075055f74ea636912760a
Chris Agold <christophersagold@gmail.com>
2021-06-24 13:42:12 -0500

    added props

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS
+import './index.css';
+// Variables
+const firstBook = {
+    img: "https://pbs.twimg.com/profile_images/1197056293963489280/rImVimA8_400x400.jpg",
+    title: 'My Hero',
+    author: 'Kirashima'
+}
 
-// Nested components, React Tools
+const secondBook = {
+    img: "https://i.pinimg.com/564x/8f/d7/a4/8fd7a404f715702524fab9ca3a48e0e9.jpg",
+    title: 'My Hero',
+    author: 'Todoroki'
+}
 
 function BookList() {
     return (
-        <section>
-            <Book/>
+        <section className='book-list'>
+            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+            <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
         </section>
     )
 }
 
-const Book = () => {
+const Book = (props) => {
     return (
-        <article>
-            <Image/>
-            <Title/>
-            <Author/>
-        </article>)
+        <article className='book'>
+            <img src={props.img} alt='My Hero'/>
+            <h1>{props.title}</h1>
+            <h4>{props.author}</h4>
+        </article>
+    )
 };
 
-const Image = () => (
-    <img src="https://pbs.twimg.com/profile_images/1197056293963489280/rImVimA8_400x400.jpg"
-         alt=''
-    />
-)
-
-const Title = () => <h1>Hello</h1>;
-const Author = () => (
-    <h4 style={{color: '#617d98', fontSize: '0.75trm', marginTop: '0.25rem'}}>
-        Kirashima
-    </h4>
-)
 ReactDOM.render(<BookList/>, document.getElementById('root'));
 
