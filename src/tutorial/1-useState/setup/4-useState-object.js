f12bb4f387ae98901db0691ac9fde5d2a9ddd8b1
Chris Agold <christophersagold@gmail.com>
2021-06-25 11:33:36 -0500

    added counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -8,16 +8,22 @@ const UseStateObject = () => {
         message: 'random message',
     })
 
-// BUTTON FUNCTION (SPREAD OPERATOR EXAMPLE)******************************
+    // MULTIPLE STATE VALUES
+    const [name, setName] = useState('peter')
+    const [age, setAge] = useState(26)
+    const [message, setMessage] = useState('random message')
+
+// ******BUTTON FUNCTION (SPREAD OPERATOR EXAMPLE, PRESERVE OTHER VALUES IN OBJECT)****** //
     const changeMessage = () => {
-        setPerson({...person, message: 'hello world'})
+        // setPerson({...person, message: 'hello world'})
+        setMessage("Hello World")
     }
 
     return (
         <>
-            <h3>{person.name}</h3>
-            <h3>{person.age}</h3>
-            <h3>{person.message}</h3>
+            <h3>{name}</h3>
+            <h3>{age}</h3>
+            <h3>{message}</h3>
             <button className="btn" onClick={changeMessage}>
                 change message
             </button>
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+    // STATE
+    const [value, setValue] = useState(0)
+    // RESET FUNCTION
+    const reset = () => {
+        setValue(0)
+    }
+
+    return (
+        <>
+            <section style={{margin: '4rem 0'}}>
+                <h2>COUNTER</h2>
+                <h1>{value}</h1>
+                <button className="btn" onClick={() => setValue(value - 1)}>DECREASE</button>
+                <button className="btn" onClick={reset}>RESET</button>
+                <button className="btn" onClick={() => setValue(value + 1)}>INCREASE</button>
+            </section>
+        </>
+    );
 };
 
 export default UseStateCounter;
