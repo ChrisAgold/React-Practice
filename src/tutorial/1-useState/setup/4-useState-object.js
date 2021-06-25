9b5820c407f170e3a30b0d37327ad6cc6eb8ab5d
Chris Agold <christophersagold@gmail.com>
2021-06-25 11:18:53 -0500

    added spread operator function to object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+    // STATE
+    const [person, setPerson] = useState({
+        name: 'peter',
+        age: 24,
+        message: 'random message',
+    })
+
+// BUTTON FUNCTION (SPREAD OPERATOR EXAMPLE)******************************
+    const changeMessage = () => {
+        setPerson({...person, message: 'hello world'})
+    }
+
+    return (
+        <>
+            <h3>{person.name}</h3>
+            <h3>{person.age}</h3>
+            <h3>{person.message}</h3>
+            <button className="btn" onClick={changeMessage}>
+                change message
+            </button>
+        </>
+    );
 };
 
 export default UseStateObject;
