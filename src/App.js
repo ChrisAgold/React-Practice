0c867f48b21a9c41908450df10af43d01b38625f
Chris Agold <christophersagold@gmail.com>
2021-09-20 12:53:07 -0500

    added blog layout for post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+import 'semantic-ui-css/semantic.min.css'
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
 
 function App() {
     return (
-        <div className='container'>
-            <Setup />
+        <div className='ui comments'>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src="" alt="profile picture"/>
+                </a>
+                <div className='content'>
+                    <a href='/' className='author'>
+                        Sarah
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>
+                            Today at 5:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        it's amazing
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
